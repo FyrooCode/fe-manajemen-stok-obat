dd53545b19a9ba6d6f90797364a61f150052517d
Bell <fyroocode@gmail.com>
2024-11-29 19:20:23 +0700

    + login with auth

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,72 @@
+<template>
+  <!--begin::Authentication Layout -->
+  <div class="d-flex flex-column flex-lg-row flex-column-fluid py-20">
+    <!--begin::Body-->
+    <div
+      class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-3 order-2 order-lg-1 justify-content-center align-items-center"
+    >
+      <!--begin::Form-->
+      <div class="d-flex flex-center flex-column flex-lg-row-fluid">
+        <!--begin::Wrapper-->
+        <div class="w-100 w-lg-500px p-3">
+          <router-view></router-view>
+        </div>
+        <!--end::Wrapper-->
+      </div>
+      <!--end::Form-->
+    </div>
+    <!--end::Body-->
+
+    <!--begin::Aside-->
+    <div
+      class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
+      :style="`background-image: url('${getAssetPath(
+        '/media/misc/auth-bg.png'
+      )}')`"
+    >
+      <!--begin::Content-->
+      <div
+        class="d-flex flex-column flex-center py-5 py-lg-15 px-3 px-md-15 w-100"
+      >
+        <!--begin::Logo-->
+        <router-link to="/" class="mb-5 mb-lg-12">
+          <img
+            alt="Logo"
+            :src="getAssetPath('media/logos/custom-1.png')"
+            class="h-60px h-lg-75px"
+          />
+        </router-link>
+        <!--end::Logo-->
+      </div>
+      <!--end::Content-->
+    </div>
+    <!--end::Aside-->
+  </div>
+  <!--end::Authentication Layout -->
+</template>
+
+<script lang="ts">
+import { getAssetPath } from "@/core/helpers/assets";
+import { defineComponent, onMounted } from "vue";
+import LayoutService from "@/core/services/LayoutService";
+import { useBodyStore } from "@/stores/body";
+
+export default defineComponent({
+  name: "auth-layout",
+  components: {},
+  setup() {
+    const store = useBodyStore();
+
+    onMounted(() => {
+      LayoutService.emptyElementClassesAndAttributes(document.body);
+
+      store.addBodyClassname("app-blank");
+      store.addBodyClassname("bg-body");
+    });
+
+    return {
+      getAssetPath,
+    };
+  },
+});
+</script>
---
+++ src/stores/auth.ts
@@ -0,0 +1,96 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import ApiService from "@/core/services/ApiService";
+import JwtService from "@/core/services/JwtService";
+
+export interface User {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+  api_token: string;
+}
+
+export const useAuthStore = defineStore("auth", () => {
+  const errors = ref({});
+  const user = ref<User>({} as User);
+  const isAuthenticated = ref(!!JwtService.getToken());
+
+  function setAuth(authUser: User) {
+    isAuthenticated.value = true;
+    user.value = authUser;
+    errors.value = {};
+    JwtService.saveToken(authUser.api_token);
+  }
+
+  function setError(error: any) {
+    errors.value = { ...error };
+  }
+
+  function purgeAuth() {
+    isAuthenticated.value = false;
+    user.value = {} as User;
+    errors.value = [];
+    JwtService.destroyToken();
+  }
+
+  function login(credentials: User) {
+    return ApiService.post("auth/login", credentials)
+      .then(({ data }) => {
+        if (data.token) {
+          setAuth({ ...data, api_token: data.token });
+        } else {
+          setError({ message: "Token not found in response" });
+        }
+      })
+      .catch(({ response }) => {
+        setError(response.data || { message: "Login failed" });
+      });
+  }
+
+  function logout() {
+    purgeAuth();
+  }
+
+  function register(credentials: User) {
+    return ApiService.post("register", credentials)
+      .then(({ data }) => {
+        setAuth(data);
+      })
+      .catch(({ response }) => {
+        setError(response.data.errors);
+      });
+  }
+
+  function forgotPassword(email: string) {
+    return ApiService.post("forgot_password", email)
+      .then(() => {
+        setError({});
+      })
+      .catch(({ response }) => {
+        setError(response.data.errors);
+      });
+  }
+
+  function verifyAuth() {
+    const token = JwtService.getToken();
+    if (token) {
+      // Token ada di localStorage, anggap pengguna terautentikasi
+      isAuthenticated.value = true;
+    } else {
+      // Token tidak ada, anggap pengguna tidak terautentikasi
+      purgeAuth();
+    }
+  }
+
+  return {
+    errors,
+    user,
+    isAuthenticated,
+    login,
+    logout,
+    register,
+    forgotPassword,
+    verifyAuth,
+  };
+});
